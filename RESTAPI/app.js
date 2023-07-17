aabe669f7c64006aea70fee0415533273634d586
Anton <kucherenko-anton1@mail.ru>
2023-07-17 15:00:35 +0300

    first commit

---
+++ RESTAPI/app.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const feedRoutes = require('./routes/feed');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+        ) {
+            cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const _dirname = require('./util/path')
+
+app.use(bodyParser.json()); //app json
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+app.use('/images', express.static(path.join(_dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
+
+mongoose.connect('mongodb+srv://anton:PLiaApfQ7vwmm0wZ@cluster0.ged35hr.mongodb.net/messages?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err))
